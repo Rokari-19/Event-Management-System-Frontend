761c1140c7c1130f8ad26f2ea72a006303e42f12
rokari-19 <oshomahiyelobu1@gmail.com>
2024-09-14 19:38:30 +0100

    first commit, axios setup complete

---
+++ src/components/Card.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: "Card",
+  data() {
+    return {
+      events : []
+    }
+  },
+  mounted() {
+    this.getEvents()
+
+  },
+  methods: {
+    getEvents() {
+      axios
+        .get("/api/v1/events/")
+        .then(response => {
+          this.events = response.data;
+          console.log(this.events);
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  }
+}
+</script>
